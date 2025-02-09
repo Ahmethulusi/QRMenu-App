5537ae7e0847e71de8663e45f49b084f932c821e
ahulusi_ymk <ahmet4112004@gmail.com>
2025-02-09 17:01:11 +0300

    asdf

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('QRMenu', 'postgres', 'Promaster1234.', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-module.exports = sequelize;
